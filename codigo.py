0e44255838e264ae678199a082e60ed714e73d6b
Nico09js <nicojara09@gmail.com>
2024-10-03 22:04:57 -0300

    Revisión de código y propuesta de mejoras

---
+++ codigo.py
@@ -0,0 +1,30 @@
+# Código a analizar
+
+def suma_numeros(lista_numeros):
+
+  suma = 0
+
+  for numero in lista_numeros:
+
+    suma =+ numero # Error lógico aquí: usa =+ en lugar de +=
+
+   
+
+  return suma
+
+
+
+# Lista de números de ejemplo
+
+numeros = [1, 2, 3, 4, 5]
+
+
+
+# Llamando a la función y mostrando el resultado
+
+resultado = suma_numeros(numeros)
+
+print(f"La suma de los números es: {resultado}")
+
+#holis
+#codigo 2
